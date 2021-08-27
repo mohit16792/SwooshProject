90488546546c1d466dcf1522685634560bc7f699
mohit <mohit16792@gmail.com>
2021-08-27 11:09:44 +0530

    League Screen make

--- app/src/main/java/com/example/swooshproject/LeagueActivity.kt
+++ app/src/main/java/com/example/swooshproject/LeagueActivity.kt
@@ -1,11 +1,11 @@
 package com.example.swooshproject
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LeagueActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_league)
     }
 }
