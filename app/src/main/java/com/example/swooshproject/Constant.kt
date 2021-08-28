d65110bf19123090bd63a7265ff1339bbf9e5270
mohit <mohit16792@gmail.com>
2021-08-28 11:36:37 +0530

    validation and intent work done

---
+++ app/src/main/java/com/example/swooshproject/Constant.kt
@@ -0,0 +1,3 @@
+package com.example.swooshproject
+
+const val EXTRA_LEAGUGE="leaguge"
--- app/src/main/java/com/example/swooshproject/LeagueActivity.kt
+++ app/src/main/java/com/example/swooshproject/LeagueActivity.kt
@@ -3,19 +3,45 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_league.*
 
 
-class LeagueActivity : AppCompatActivity() {
-
+class LeagueActivity : BaseActivity() {
+    var selectedLeaguge=""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
 
 
+
     }
+    fun mensLeagugeClickec(view: View) {
+       womensLeagugeBtn.isChecked=false
+       coedLeagugeBtn.isChecked=false
+        selectedLeaguge="mens"
+    }
+    fun womensLeagugeClicked(view: View) {
+        mensLeagugeBtn.isChecked=false
+        coedLeagugeBtn.isChecked=false
+        selectedLeaguge="womens"
+    }
+    fun coedLeaugeClicked(view: View) {
+        womensLeagugeBtn.isChecked=false
+        mensLeagugeBtn.isChecked=false
+        selectedLeaguge="coed"
+    }
+
     fun leagueNextClicked(view: View) {
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+        if(selectedLeaguge!="") {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUGE,selectedLeaguge)
+            startActivity(skillActivity)
+        }else{
+            Toast.makeText(this,"please select a Leauge",Toast.LENGTH_SHORT).show()
+        }
     }
+
+
 }
